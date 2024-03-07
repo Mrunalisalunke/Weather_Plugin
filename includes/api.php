1e9a001528dfd99498e914ca3604628d85a7fb32
Mrunali Salunke <mrunalisalunke@Mrunalis-Laptop.local>
2024-03-07 16:28:42 +0530

    changes

--- includes/api.php
+++ includes/api.php
@@ -14,46 +14,37 @@
  *
  * @return string Weather data or error message.
  */
-function Get_Weather_data()
+function get_weather_data()
 {
-    $latitude = floatval(get_option('latitude'));
-    $longitude = floatval(get_option('longitude'));
+    $latitude = get_option('latitude');
+    $longitude = get_option('longitude');
 
     if (empty($latitude) || empty($longitude)) {
         return __('Error: Latitude and Longitude must be set in plugin options.', 'weather-translate');
     }
 
-    $api_key = '75db5d2cafaff28402711d4266fa7164';
-    $api_url = "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$api_key";
+    $api_url = "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=75db5d2cafaff28402711d4266fa7164";
 
     $response = wp_remote_get($api_url);
 
     if (is_wp_error($response)) {
-        return __('Error: Unable to fetch weather data from OpenWeatherMap API.', 'weather-translate');
+        return __('Error fetching data from OpenWeatherMap API.', 'weather-translate');
     }
 
     $data = json_decode(wp_remote_retrieve_body($response), true);
 
     if (isset($data['cod']) && $data['cod'] === 200) {
-        $temperature = isset($data['main']['temp']) ? $data['main']['temp'] : __('N/A', 'weather-translate');
-        $description = isset($data['weather'][0]['description']) ? $data['weather'][0]['description'] : __('N/A', 'weather-translate');
-        $city = isset($data['name']) ? $data['name'] : __('N/A', 'weather-translate');
-        $country = isset($data['sys']['country']) ? $data['sys']['country'] : __('N/A', 'weather-translate');
+        $temperature = isset($data['main']['temp']) ? $data['main']['temp'] : 'N/A';
+        $description = isset($data['weather'][0]['description']) ? $data['weather'][0]['description'] : 'N/A';
+        $city = isset($data['name']) ? $data['name'] : 'N/A';
+        $country = isset($data['sys']['country']) ? $data['sys']['country'] : 'N/A';
 
         $output = __("Latitude: %s<br>Longitude: %s<br>City: %s<br>Country: %s<br>Temperature: %s°C<br>Description: %s", 'weather-translate');
 
-        $output = sprintf(
-            $output,
-            esc_html($latitude),
-            esc_html($longitude),
-            esc_html($city),
-            esc_html($country),
-            esc_html($temperature),
-            esc_html($description)
-        );
+        $output = sprintf($output, esc_html($latitude), esc_html($longitude), esc_html($city), esc_html($country), esc_html($temperature), esc_html($description));
 
         return $output;
     } else {
-        return __('Error: Unable to fetch weather data. Check your OpenWeatherMap API key and try again.', 'weather-translate');
+        return __('Error: Unable to fetch weather data.', 'weather-translate');
     }
 }
